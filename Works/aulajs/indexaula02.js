df5f1147884f10157cd310055e1c6c3ac254d815
eduardo-120 <56237939+eduardo-120@users.noreply.github.com>
2020-05-31 18:06:30 -0300

    aula02

---
+++ Works/aulajs/indexaula02.js
@@ -0,0 +1,19 @@
+// cria uma programa que caucula a média
+// as notas entre os alunos e envia
+// mensagem do cálculo da média.
+
+const aluno01 = 'Duzão';
+const aluno02 = 'Duzilho';
+const aluno03 = 'Du';
+
+const notaaluno01 = 10.0;
+const notaaluno02 = 10.5;
+const notaaluno03 = 11.0;
+
+const média = (notaaluno01 + notaaluno02 + notaaluno03) / 3
+
+console.log(aluno01, notaaluno01);
+console.log(aluno02, notaaluno02);
+console.log(aluno03, notaaluno03);
+
+console.log('Meias dos alunos no total ', média);
